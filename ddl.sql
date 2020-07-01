cac94483038ca5a1d9ff2bca4b2cabe015d89c1b
olivia <wangol@oocl.com>
2020-07-01 19:16:34 +0800

    commit

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,76 @@
 -- **Database level:**
--- Display all the database
+-- Display all the database\
+
+SHOW DATABASES;
+
 
 -- Enter a certain database
 
+USE sql_ddl;
+
 -- Create a database
+
+CREATE DATABASE olivia;
 
 -- Create the database of the designated character set
 
+CREATE DATABASE `base2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 -- Display the creation information fo the database
+
+SHOW  CREATE DATABASE olivia;
 
 -- Revise the codes of the database
 
+ALTER DATABASE olivia CHARSET=gbk COLLATE=gbk_chinese_ci;
+
 -- Delete a database
+
+DROP DATABASE olivia;
 
 -- **Table level**
 -- Revise table name
 
+CREATE TABLE student
+(
+	id VARCHAR(3) PRIMARY KEY,
+	NAME VARCHAR(10),
+	age INT,
+	sex VARCHAR(5)
+)
+RENAME TABLE student TO student1;
+
+
 -- Revise the field's data type
+
+ALTER TABLE student1 USER MODIFY sex VARCHAR(100);
 
 -- Revise field name
 
+ALTER TABLE student1 CHANGE age studentAge INT;
+
 -- Add field
+
+ALTER TABLE student1 ADD hobby VARCHAR(30）;
 
 -- Delete field
 
+ALTER TABLE student1 DROP hobby;
+
 -- Revise the table's storage engine
+
+ALTER TABLE student1 ENGINE=INNODB;
 
 -- Delete the table's foreign key restriant
 
+ALTER TABLE student1 ADD
+CONSTRAINT student_class
+FOREIGN KEY (id)
+REFERENCE class(id)
+ON DELETE CASCADE;
+
+ALTER TABLE student1 DROP FOREIGN KEY student_class;
+
 -- Delete a table
+
+DROP TABLE student1;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,23 @@
 -- Insert record
 
+CREATE TABLE student
+(
+	id VARCHAR(3) NOT NULL PRIMARY KEY,
+	NAME VARCHAR(10) NOT NULL,
+	age INT NOT NULL,
+	sex VARCHAR(6) NOT NULL
+)
+INSERT INTO student(id,NAME,age,sex) VALUES('001','Colin',18,'male'),('002','Emma',20,'female');
+
+
 -- Revise record
+
+UPDATE student SET age=20 WHERE sex='male';
 
 --  Delete record
 
+DELETE FROM student WHERE NAME='emma';
+
 -- Search record
+
+SELECT * FROM student WHERE NAME='Colin';
